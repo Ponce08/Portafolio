6fa15443805c99233b7266ab458520e178975bfb
ponce08 <yonathanponce2019@gmail.com>
2025-01-14 15:21:21 -0500

    modified about 2

--- src/components/about_2/about_2.jsx
+++ src/components/about_2/about_2.jsx
@@ -6,20 +6,20 @@ export default function About_2({ mode }) {
   return (
     <div className="content_main_about2">
       <div className={mode ? 'content_p_logo' : 'content_p_logo dark_p_h1'}>
-        <div className='content_p'>
+        <div className="content_p">
           <h1 className="h1_about_2" id="sobre mi">
             <img src={me_icon} alt="" className="me_icon" />
             Sobre mi
           </h1>
           <p>
-            Me llamo Yonathan Ponce pero mis amigos me dicen "Ponce". Despues de dedicar casi toda mi vida a la logistica y a la
-            distribución en empresas de producción, en julio del 2023 nació en mí un interes por la tecnología, desde entonces soy
-            un programador que esta en continuo aprendizaje y disfruto usar mis habilidades para contribuir a proyectos
+            Me llamo Yonathan Ponce pero mis amigos me dicen solo "Ponce". Despues de dedicar casi toda mi vida a la logistica y a
+            la distribución en empresas de producción, en julio del 2023 nació en mí un interes por la tecnología, desde entonces
+            soy un programador que esta en continuo aprendizaje y disfruto usar mis habilidades para contribuir a proyectos
             tecnológicos. En mi Stack actual se encuentra:
             <span>
               {' '}
-              HTML, CSS, javaScript, React js, Redux, Zustand, Node js, Express js, Sequelize, Graphql, Supabase, MongoDB y
-              PostgreSQL.
+              HTML, CSS, javaScript, Typescript, React js, Redux, Zustand, Node js, Express js, Sequelize, Graphql, Supabase,
+              MongoDB y PostgreSQL.
             </span>{' '}
             Me caracterizo por ser una persona proáctiva y autónoma. Soy un hombre apasionado por la musica, los viajes y la
             comida tipica de mi pais Colombia. Me gustan los retos y el trabajo en equipo.
